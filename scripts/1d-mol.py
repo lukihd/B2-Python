0f9ac5f753f57cf72fc12d5bc3961ac8e7de7130
lukihd <lucas.erisset@ynov.com>
2018-10-23 17:28:29 +0200

    .\scripts\1d-mol.py

--- scripts/1d-mol.py
+++ scripts/1d-mol.py
@@ -1,7 +1,57 @@
+#!/usr/bin/python3.6
+# nom : 1d-mol.py
+# auteur : lucas Erisset
+# date : 23/10/18
 # jeux du plus ou moins en Python
 
+import re
 import random
 
-a = random.randint(0,9)
+def restart ():
+    restartValue = input("Voulez vous rejouer oui(o)/non(n) : ")
+    if restartValue is "o" or restartValue is "O":
+        return True
+    elif restartValue is "n" or restartValue is "N":
+        return False
+    else:
+        return print("Va te payer un café au lieu de tout casser") 
 
-print(a)
+
+def checkType (valueToCheck):
+    if re.match('^[0-9]+$', valueToCheck):
+        valueToCheck = int(valueToCheck)
+        if valueToCheck > 100:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+def checkExit (valueToCheck):
+    if valueToCheck == "q" or valueToCheck == "Q":
+        print ("Au revoir, la solution était : " + str(intToFind))
+        return exit()
+
+restartGame = True
+
+while restartGame == True:
+    intToFind = random.randint(0, 100)
+    print(intToFind)
+
+    inputUsr = input("Entrez un chiffre entre 0 et 100 : ")
+    checkExit(inputUsr)
+
+    while inputUsr != intToFind:
+
+        while checkType(inputUsr) == False:
+            inputUsr = input("Vous devez entrer un chiffre entre 0 et 100 (pas des lettres ou autres) : ")
+            checkExit(inputUsr)
+    
+        inputUsr = int(inputUsr)
+        if inputUsr > intToFind:
+            inputUsr = input("Moins : ")
+        elif inputUsr < intToFind:
+            inputUsr = input("Plus : ")
+        
+    print("Vous avez gagné !")
+    restartGame = restart()
