a86d931bd3cb1e9bdb3d310c65bacd325f99e149
lukihd <lucas.erisset@ynov.com>
2018-11-05 22:24:00 +0100

    .\2a-mol\2a-mol.py

--- scripts/2a-mol/2a-mol.py
+++ scripts/2a-mol/2a-mol.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # nom : 2a-mol.py
 # auteur : lucas Erisset
-# date : 01/11/18
-# jeux du plus ou moins en Python qui inscrit dans un document
+# date : 04/11/18
+# jeux du plus ou moins en Python qui inscrit dans un document cible ( ne fonctionne pas à cause des droits sur mol.txt que je n'arrive pas a rendre 'write' )
 
 import re
 import random
 import signal
 
-
+# recommencer le jeu a la fin de la partie
 def restart():
     WriteIn("Voulez vous rejouer oui(o)/non(n) : ")
     restartValue = ReadIn()
@@ -17,9 +17,9 @@ def restart():
     elif restartValue is "n" or restartValue is "N":
         return False
     else:
-        return print("Va te payer un café au lieu de tout casser")
+        return WriteIn("Va te payer un café au lieu de tout casser")
 
-
+# regex pour vérifier un int
 def checkType(valueToCheck):
     if re.match('^[0-9]+$', valueToCheck):
         valueToCheck = int(valueToCheck)
@@ -30,26 +30,26 @@ def checkType(valueToCheck):
     else:
         return False
 
-
+# verification de la sortie du script
 def checkExit(valueToCheck):
     if valueToCheck == "q" or valueToCheck == "Q":
         print("Au revoir, la solution était : " + str(intToFind))
         return exit()
 
-
+# ecris dans le document cible la valeur en parametre
 def WriteIn(valueToSend):
     fileTxt = open("mol.txt", "w")
     print(valueToSend, fileTxt)
     fileTxt.close()
 
-
+# lis le document cible
 def ReadIn():
     fileTxt = open("mol.txt", "r")
     valueToRead = fileTxt.read()
     fileTxt.close()
     return valueToRead
 
-
+# signaux d'arret du script
 def checkSignal(sig, frame):
     print("Au revoir, la solution était : " + str(intToFind))
     return exit()
@@ -60,6 +60,7 @@ signal.signal(signal.SIGTERM, checkSignal)
 
 restartGame = True
 
+# le jeu
 while restartGame == True:
     intToFind = random.randint(0, 100)
     print(intToFind)
